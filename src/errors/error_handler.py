a6ab0df1ac16466e664225b84054d3281a028c41
Gabriel Alves <jgabrielalves@alu.ufc.br>
2024-02-08 11:24:19 -0300

    feat: Implementing Validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,6 +1,18 @@
 from src.views.http_types.http_response import HttpResponse
+from src.errors.erro_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 def handle_erros(error: Exception):
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(
+            status_code = error.status_code,
+            body= {
+                "erros": [{
+                    "title": error.name,
+                    "detail": error.message
+                }]
+            }
+        )
+
     return HttpResponse(
         status_code=500,
         body= {
